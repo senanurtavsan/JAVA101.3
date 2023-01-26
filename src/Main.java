11cb074d705ac1db941aa5b1feee726a200cd455
senanurtavsan <122603342+senanurtavsan@users.noreply.github.com>
2023-01-26 15:18:59 +0300

    Son Hali

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
+       Scanner inp = new Scanner(System.in);
 
 
         System.out.print("Gidilen Yolun Uzunluğu : ");
         double mesafe=inp.nextDouble();
 
-        double ucret = mesafe * 2.20;
+        double ucret = (mesafe * 2.20)+10;
 
-        double tutar = ucret < 20 ? (20) : (ucret*2.20);
+        double tutar = ucret < 20 ? (20) : ((mesafe*2.20)+10);
 
         System.out.println(mesafe);
         System.out.println("Ücret : " + tutar + "-TL");
+
 
 
 
